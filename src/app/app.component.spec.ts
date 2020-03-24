5f26fc8ecd2244e4a4e462619ad3e8a93a835e48
NPGonzalez <naypili@gmail.com>
2020-03-24 15:46:22 -0300

    Cambio configuración proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projectHere'`, () => {
+  it(`should have as title 'mapas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projectHere');
+    expect(app.title).toEqual('mapas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('projectHere app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('mapas app is running!');
   });
 });
