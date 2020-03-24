fef86a5e04e6ba2a884feae30a308c2001efa0f7
Nayarett González <43223906+NPGonzalez@users.noreply.github.com>
2020-03-24 16:00:25 -0300

    Revert "Se agrega componente mapa; modifica configuración"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { MaterialModule } from './material.module';
-import { HereMapComponent } from './components/here-map/here-map.component';
-
-
 @NgModule({
   declarations: [
-    AppComponent,
-    HereMapComponent,
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
